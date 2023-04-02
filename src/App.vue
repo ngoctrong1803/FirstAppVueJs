13958f658e61e452794dfac1ac66e3b6ae461880
ngoctrong1803 <ngoctrong1412@gmail.com>
2023-04-02 22:02:44 +0700

    first vuejs app

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<!-- eslint-disable vue/valid-v-bind -->
+<!-- eslint-disable max-len -->
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+<template>
+    <h1>{{ title }}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">click me!</button>
+    <div v-if="showModal">
+        <Modal :title_prop="title_prop"
+              text="this property text is a prop"
+              v-bind:data="['dataString', 4, {'keya':'valuea'}]"
+              theme="dynamic_class"
+              @close="toggleModal">
+              <template v-slot:links>
+                <a href="#">link to website</a>
+              </template>
+              <div>
+                <h1>this is learning about slots</h1>
+              </div>
+        </Modal>
+    </div>
+
+    <Teleport to=".modals" v-if="showModal2">
+        <Modal :title_prop="title_prop"
+              text="this property text is a prop"
+              v-bind:data="['dataString', 4, {'keya':'valuea'}]"
+              theme="dynamic_class"
+              @close="toggleModal">
+              <template v-slot:links>
+                <a href="#">link to website</a>
+              </template>
+              <div>
+                <h1>this is learning about slots</h1>
+              </div>
+        </Modal>
+    </Teleport>
+    <button @click="toggleModal">Show Modal</button>
+    <button @click="toggleModalTwo">Show Modal 2</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: 'My First We',
+      // prop
+      title_prop: 'this property is a prop and it will pass into modal',
+      showModal: false,
+      showModal2: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,67 @@
+<!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
+<template>
+  <!-- @click.self is one of 'click event modifiers' -->
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{sale : theme === 'dynamic_class'}">
+      <h1>Hello this is the modal 2</h1>
+      {{ title_prop }}
+      {{ text }}
+      <p>data array</p>
+      {{ data }}
+      <slot></slot>
+      <slot name="links"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Modal',
+  props: ['title_prop', 'text', 'data', 'theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.modal{
+  width: 400px;
+  height: 300px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop{
+  top: 0;
+  position: fixed;
+  background: rgba(227, 219, 219, 0.4);
+  width: 100%;
+  height: 100%;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+h1{
+  color: aqua;
+}
+.modal.sale{
+  color: red;
+}
+</style>
